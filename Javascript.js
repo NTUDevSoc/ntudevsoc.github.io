bb525ffbad1fb25bde4129defb797de6e091cee5
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2020-09-18 21:54:14 +0100

    Website Template Update

--- Javascript.js
+++ Javascript.js
@@ -56,17 +56,33 @@ window.onload = function() {
 };
 
 
-/* Warning for closed events */
-$('#warning').click(function (e) {
-    e.preventDefault()
-    $('#messageWarning').html('<div class="alert alert-danger fade-in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button><strong>Alert:</strong> This event has ended. Try another one! :)</div>');
-    })
+// Countdown Timer
+// Set the date we're counting down to
+var countDownDate = new Date("Sept 28, 2020 09:00:00").getTime();
 
-/* Warning for pending events */
-$('#pending').click(function (e) {
-    e.preventDefault()
-    $('#messagePending').html('<div class="alert alert-success fade-in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button><strong>Alert:</strong> Event details are pending. Try again later! :)</div>');
-    })
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
