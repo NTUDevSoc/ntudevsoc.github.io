aca19d78221e6ac431c3e716d9d7408f97a5e2c5
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2020-11-23 18:23:11 +0000

    Removed unused functions

--- Javascript.js
+++ Javascript.js
@@ -1,3 +1,4 @@
+// [}EVSOC_ logo on index.html animation 
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
@@ -55,68 +56,15 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
- 
-// Countdown Timer
-// Set the date we're counting down to
-// Current not in use
-var countDownDate = new Date("Sept 28, 2020 09:30:00").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-    
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-    
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
-  // Output the result in an element with id="demo"
-  if(document.getElementById("demo")){
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-    // If the count down is over, write some text 
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-  }
-    
-
-}, 1000);
-
-/* Warning for closed events */
+// Warning for closed events 
 $('#warning').click(function (e) {
   e.preventDefault()
   $('#messageWarning').html('<div class="alert alert-danger fade-in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button><strong>Alert:</strong> This event has ended. Try another one! :)</div>');
   })
 
-/* Warning for pending events */
+// Warning for pending events 
 $('#pending').click(function (e) {
   e.preventDefault()
   $('#messagePending').html('<div class="alert alert-success fade-in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button><strong>Alert:</strong> Event details are pending. Try again later! :)</div>');
   })
-
-  /* More events function */
-  function moreFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      document.getElementById("myBtn").classList.remove('fa-arrow-up');
-      document.getElementById("myBtn").classList.add('fa-arrow-down');
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      document.getElementById("myBtn").classList.remove('fa-arrow-down');
-      document.getElementById("myBtn").classList.add('fa-arrow-up');
-      moreText.style.display = "inline";
-    }
-  } 
