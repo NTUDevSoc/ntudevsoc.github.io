44a697ed36d1c915ab63535422b2da0c79666592
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-07-11 20:04:52 +0100

    Season1 content now loads automatically (DND specific javascript file created)

---
+++ Dnd/dndJavascript.js
@@ -0,0 +1,56 @@
+function showSeason(item) {
+    document.getElementById("dropdownMenu1").innerHTML = item.innerHTML;
+    displaySeasonContent();
+  }
+  
+  function getSelectedSeasonNumber() {
+    const season_title = document.getElementById("dropdownMenu1").innerHTML
+    const season_number = season_title.split(":")[0]
+    return season_number.replace(/\s+/g, '')
+  }
+  
+  function getSeasonContent() {
+    const season_number = getSelectedSeasonNumber();
+    const path = "EpisodeData/" + season_number + ".json"
+    return fetch(path).then(function(response) {
+      return response.json();
+    }).then(function(json) {
+      return json;
+    });
+  }
+  
+  function clearEpisodes() {
+    const row = document.getElementById('row-of-cards');
+    if (row !== null) row.remove();  
+  }
+  
+  async function displaySeasonContent(){
+    clearEpisodes();
+    const row_div = document.createElement("div");
+    row_div.className="row";
+    row_div.id = "row-of-cards" 
+    row_div.style.cssText="color: black;"
+  
+    const data = await getSeasonContent();
+    data.forEach(res => {
+      const column_div = document.createElement("div");
+      column_div.className="col-lg-3 col-md-6";
+      column_div.innerHTML =
+      `
+      <div class="dnd-card my-5">
+        <img class="card-img-top" src="${res.thumbnail}" alt="thumbnail">
+  
+        <div class="card-body">
+            <h5 class="card-title">${res.episodeNumber}. ${res.title}</h5>
+            <p class="card-text">
+              ${res.description}
+            </p>
+        </div>
+      </div>
+      `
+      row_div.appendChild(column_div);
+  
+      const container = document.querySelector("#container");
+      container.appendChild(row_div);
+    });
+  }
