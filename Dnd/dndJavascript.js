650526fb9cc996eda595a588395010f1e6146f79
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-07-23 15:51:09 +0100

    Minor readability improvements (Separated DND and main page CSS)

--- Dnd/dndJavascript.js
+++ Dnd/dndJavascript.js
@@ -3,6 +3,11 @@ function showSeason(item) {
     clearEpisodes();
     displaySeasonContent();
   }
+
+  function clearEpisodes() {
+    const episodes = document.querySelectorAll('[id=dnd-episode]');
+    episodes.forEach(episode => episode.remove())
+  }
   
   function getSelectedSeasonNumber() {
     const season_title = document.getElementById("seasonDropDownMenu").innerHTML
@@ -18,26 +23,21 @@ function showSeason(item) {
     });
   }
   
-  function clearEpisodes() {
-    const episodes = document.querySelectorAll('[id=dnd-episode]');
-    episodes.forEach(episode => episode.remove())
-  }
-  
   async function displaySeasonContent(){
-    const data = await getSeasonContent();
-    data.forEach(res => {
+    const episodeData = await getSeasonContent();
+    episodeData.forEach(episode => {
       const column_div = document.createElement("div");
       column_div.className="col-xl-3 col-lg-4 col-md-6";
       column_div.innerHTML =
       `
       <div class="dnd-card my-5" id="dnd-episode">
-        <img class="card-img-top" src="${res.thumbnail}" alt="thumbnail">
+        <img class="card-img-top" src="${episode.thumbnail}" alt="thumbnail">
   
         <div class="card-body">
-            <h5 class="card-title">${res.episodeNumber}. ${res.title}</h5>
-            <p class="card-text">${res.description} </p>
+            <h5 class="card-title">${episode.episodeNumber}. ${episode.title}</h5>
+            <p class="card-text">${episode.description} </p>
         </div>
-        <a href="${res.link}" class="stretched-link" target="_"></a>
+        <a href="${episode.link}" class="stretched-link" target="_"></a>
       </div>
       `
       const container = document.getElementById("row-of-cards");
