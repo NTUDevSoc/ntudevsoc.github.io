9e14f33deacb7d8166dd6916a29cc08bc698f62a
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-07-24 15:54:24 +0100

    Addressed comments in pull request 6

--- Dnd/dndJavascript.js
+++ Dnd/dndJavascript.js
@@ -3,23 +3,25 @@
       document.getElementById("seasonDropDownMenu").innerHTML = item.innerHTML;
       clearEpisodes();
     }
-    const season_number = getSelectedSeasonNumber();
+    const season_number = await getSelectedSeasonNumber();
     const episodeData = await getSeasonContent(season_number);
     displaySeasonContent(episodeData);
   }
 
   function clearEpisodes() {
-    const episodes = document.querySelectorAll('[id=dnd-column]');
-    episodes.forEach(episode => episode.remove())
+    const episodes = document.getElementsByClassName("dnd-column");
+    for (var i = episodes.length-1; i >= 0; i--) {
+      episodes[i].remove();
+    }
   }
   
-  function getSelectedSeasonNumber() {
+  async function getSelectedSeasonNumber() {
     const season_title = document.getElementById("seasonDropDownMenu").innerHTML
     const season_number = season_title.split(":")[0]
     return season_number.replace(/\s+/g, '')
   }
   
-  function getSeasonContent(season_number) {
+  async function getSeasonContent(season_number) {
     const path = "EpisodeData/" + season_number + ".json"
     return fetch(path).then(function(response) {
       return response.json();
@@ -30,11 +32,10 @@
     const container = document.getElementById("row-of-cards");
     episodeData.forEach(episode => {
       const column_div = document.createElement("div");
-      column_div.className="col-xl-3 col-lg-4 col-md-6";
-      column_div.id="dnd-column";
+      column_div.className="dnd-column col-xl-3 col-lg-4 col-md-6";
       column_div.innerHTML =
       `
-      <div class="my-5" id="dnd-episode">
+      <div class="my-5">
         <img class="card-img-top" src="${episode.thumbnail}" alt="Episode ${episode.episodeNumber} thumbnail">
   
         <div class="card-body">
